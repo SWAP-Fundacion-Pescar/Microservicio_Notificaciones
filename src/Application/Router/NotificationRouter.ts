e2b48125945770f1ced90b682976ae6ca1773606
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-07 22:50:34 -0300

    Validation Error Handler agregado a las rutas

--- src/Application/Router/NotificationRouter.ts
+++ src/Application/Router/NotificationRouter.ts
@@ -5,6 +5,7 @@ import NotificationQuery from '../../Infrastructure/Query/NotificationQuery';
 import NotificationCommand from '../../Infrastructure/Command/NotificationCommand';
 import { authenticateJwt } from '../Middleware/PassportMiddleware';
 import {validateCreateNotification, validateReadNotification, validateDeleteNotification} from '../Middleware/Validator/NotificationValidator'
+import validationErrorHandler from '../Middleware/Validator/ValidationErrorHandler';
 
 const router = Router();
 const command = new NotificationCommand();
@@ -12,8 +13,8 @@ const query = new NotificationQuery();
 const notificationServices = new NotificationServices(command, query);
 const controller = new NotificationController(notificationServices);
 
-router.post('/notification', validateCreateNotification, authenticateJwt, controller.createNotification);
-router.delete('/notification/:notificationId', validateDeleteNotification, authenticateJwt, controller.deleteNotification);
-router.put('/notification', validateReadNotification, authenticateJwt, controller.readNotification);
+router.post('/notification', validateCreateNotification, validationErrorHandler, authenticateJwt, controller.createNotification);
+router.delete('/notification/:notificationId', validateDeleteNotification, validationErrorHandler, authenticateJwt, controller.deleteNotification);
+router.put('/notification', validateReadNotification, validationErrorHandler, authenticateJwt, controller.readNotification);
 
 export default router;
