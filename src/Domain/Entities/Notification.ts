7acc9f15ca69ed1fe4212978e117d64fb9789b99
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-23 00:50:52 -0300

    Correccion de entidad Notification

--- src/Domain/Entities/Notification.ts
+++ src/Domain/Entities/Notification.ts
@@ -4,16 +4,18 @@ class Notification {
     isRead: boolean
     receivedAt: Date
     createdAt: Date
-    content: string 
+    content: Object 
+    message: string
     type: string
     hasImage?: boolean
-    constructor(id:string, userId: string, isRead: boolean, receivedAt: Date, createdAt: Date, content: string, type: string, hasImage?: boolean){
+    constructor(id:string, userId: string, isRead: boolean, receivedAt: Date, createdAt: Date, content: Object, message: string, type: string, hasImage?: boolean){
         this.id = id;
         this.userId = userId;
         this.isRead = isRead;
         this.receivedAt = receivedAt;
         this.createdAt = createdAt;
         this.content = content;
+        this.message = message;
         this.hasImage = hasImage;
         this.type = type;
     }
