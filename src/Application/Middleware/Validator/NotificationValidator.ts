61699bb8f6a37f796026ff9660639ca1f4d90878
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-23 00:56:52 -0300

    Correcion de validacion

--- src/Application/Middleware/Validator/NotificationValidator.ts
+++ src/Application/Middleware/Validator/NotificationValidator.ts
@@ -2,7 +2,8 @@ import {body, param} from 'express-validator';
 
 const validateCreateNotification = [
     body('userId').isString().withMessage('User ID debe ser un string'),
-    body('content').isString().withMessage('Content debe ser un string'),
+    body('content').isObject().withMessage('Content debe ser un objecto'),
+    body('message').isString().withMessage('Message debe ser un string'),
     body('type').isString().withMessage('Type debe ser un string'),
     body('hasImage').isBoolean().withMessage('Has image debe ser un boolean')
 ]
